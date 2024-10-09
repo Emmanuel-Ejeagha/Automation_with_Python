83ec43db601aa00ddc061c9f195d6b5b4eb47855
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-09 03:03:48 +0100

    restful api

--- RESTFUL_API/01_restApi.py
+++ RESTFUL_API/01_restApi.py
@@ -1,10 +1,10 @@
 import requests
 
-api = requests.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=dba09d7beed146b9afd2dd98d219d3c3')
+api = requests.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=dba09d7beed146b9afd2dd98d219d3c3')
 content = api.json()
 
 print(content['articles'], "\n")
-print(content['articles'][0]['title'], "\n")
-print(content['articles'][0]['description'])
+# print(content['articles'][0]['title'], "\n")
+# print(content['articles'][0]['description'])
 
 # print(type(content))
