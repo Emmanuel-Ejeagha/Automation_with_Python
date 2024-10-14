0d74fdd5a23860719834c03119a28a5200cac073
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-14 03:34:44 +0100

    modified

--- workingWithFiles/intro.py
+++ workingWithFiles/intro.py
@@ -11,3 +11,14 @@ p1 = Path('files/w.txt')
 if not p1.exists():
     with open(p1, 'w') as file:
         file.write('Content 3')
+
+
+print(p1.name)
+print(p1.stem)
+print(p1.suffix)
+
+p2 = Path('files')
+print(p2.iterdir(), '\n')
+print(list(p2.iterdir()))
+for item in p2.iterdir():
+    print(type(item))
