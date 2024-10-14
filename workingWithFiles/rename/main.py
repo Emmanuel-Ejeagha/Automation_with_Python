ab584c3716e563f337593412410167becb52d4e3
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-14 17:59:28 +0100

    Added: appended the folder's name to files

---
+++ workingWithFiles/rename/main.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+base_dir = Path('rename')
+file_paths = base_dir.glob("**/*")
+
+# Rename the files by adding the attaching the folder's name
+for path in file_paths:
+    if path.is_file():
+        parent_folder = path.parts[-2]
+        new_filename = parent_folder + '_' + path.name
+        print(new_filename)
+        new_filepath = path.with_name(new_filename)
+        path.rename(new_filepath)
