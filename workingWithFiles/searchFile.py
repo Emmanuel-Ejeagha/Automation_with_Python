cfdce613cdcb0bb3a10beecc1f8241bd13c8c50c
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-15 20:07:23 +0100

    Added: search file in computer

--- workingWithFiles/searchFile.py
+++ workingWithFiles/searchFile.py
@@ -3,6 +3,8 @@ from pathlib import Path
 base_dir = Path('.')
 search_value = '14'
 
+# search for a file in a dir
 for path in base_dir.rglob("*"):
-    if search_value in path.stem:
-        print(path.absolute())
+    if path.is_file():
+        if search_value in path.stem:
+            print(path.absolute())
