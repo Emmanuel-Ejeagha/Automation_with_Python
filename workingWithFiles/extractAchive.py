51b2e39628caaef6e2010de6d774560d22ed2abd
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-15 11:49:04 +0100

    Added: extract achieve

--- workingWithFiles/extractAchive.py
+++ workingWithFiles/extractAchive.py
@@ -17,19 +17,19 @@ base_dir = Path('.')
 destination_path = Path('extractFiles')
 
 # Create the destination directory if it doesn't exist
-destination_path.mkdir(exist_ok=True)
+# destination_path.mkdir(exist_ok=True)
 
 # Iterate through all ZIP files in the base directory
 for path in base_dir.glob("*.zip"):
-    with zipfile.ZipFile(path, 'r') as zipF:
+    with zipfile.ZipFile(path, 'r') as zf:
         # Create a final path for extraction
         final_path = destination_path / Path(path.stem)
         
         # Create the final path directory if it doesn't exist
-        final_path.mkdir(exist_ok=True)
+        # final_path.mkdir(exist_ok=True)
         
         # Extract all contents to the final path
-        zipF.extractall(path=final_path)
+        zf.extractall(path=final_path)
         print("Done")
 
 
