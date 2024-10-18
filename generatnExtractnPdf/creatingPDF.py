f4fe21139c627335f2debf787e922d0bca722a8f
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-10-18 03:33:42 +0100

    Added: more text "

--- generatnExtractnPdf/creatingPDF.py
+++ generatnExtractnPdf/creatingPDF.py
@@ -5,5 +5,10 @@ pdf = FPDF(orientation='P', unit='pt', format='A4')
 pdf.add_page()
 
 pdf.image('generatnExtractnPdf/img.png', w=80, h=50)
+pdf.set_font(family='Times', style='B', size=24)
+pdf.cell(w=0, h=50, txt='ALX GRADUATION', align='C', border=1, ln=1)
+
+pdf.set_font(family='Times', style='B', size=24)
+pdf.cell(w=0, h=50, txt='Description:')
 
 pdf.output('generatnExtractnPdf/output.pdf')
